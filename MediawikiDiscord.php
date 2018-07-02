88bfaa695c9dd23391ce2c647266ab644c6744bd
Szmyk <szymonszmykzak@gmail.com>
2018-07-02 15:33:05 +0200

    Initial commit

---
+++ MediawikiDiscord.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+final class MediawikiDiscordHooks 
+{
+	static function onPageContentSaveComplete ($wikiPage, $user)
+	{		
+		$message = "User `" . $user . "` saved changes on page `" . $wikiPage->getTitle()->getFullText() . "`";		
+		
+		DiscordNotifications::Send($message);
+	}
+}
+
+final class DiscordNotifications
+{
+	static function Send ($message)
+	{
+		global $wgDiscordWebhookUrl;
+
+		$content = '{ "content": "' . $message . '" }';
+				
+		$data = array
+		(
+			'http' => array
+			(				
+				'method'  => 'POST',
+				'content' => $content
+			)
+		);
+
+		file_get_contents($wgDiscordWebhookUrl, false, stream_context_create($data));	
+	}
+}
+
+?>
